1d38c88a3665f0ddb9e22808f3f6f93b7063edf0
SergeyLysenko <35398518+SergeyLysenko@users.noreply.github.com>
2024-01-20 07:52:10 +0700

    Ver 0.01

---
+++ sp_project.py
@@ -0,0 +1,8 @@
+In [1]: import pandas as pd
+   ...:
+   ...: james_bond_tables = pd.read_html(
+   ...:     "https://en.wikipedia.org/wiki/List_of_James_Bond_novels_and_short_stories"
+   ...: )
+   ...: james_bond_data = james_bond_tables[1].convert_dtypes()
+
+
