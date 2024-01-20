dcb85431cc14ee77bbb8e2e937b9c1e49a291ef5
SergeyLysenko <35398518+SergeyLysenko@users.noreply.github.com>
2024-01-20 09:19:04 +0700

    Ver 0.03

--- sp_project.py
+++ sp_project.py
@@ -1,6 +1,42 @@
-In [1]: import pandas as pd
+import pandas as pd
 
- james_bond_tables = pd.read_html(
-      "https://en.wikipedia.org/wiki/List_of_James_Bond_novels_and_short_stories"
- )
- james_bond_data = james_bond_tables[1].convert_dtypes()
+# Load all tables from the URL into a list
+south_park_tables = pd.read_html(
+      "https://en.wikipedia.org/wiki/List_of_South_Park_episodes"
+)
+
+# Select and concatenate the specified tables
+selected_tables = [south_park_tables[i] for i in range(1, 27)]
+south_park_data = pd.concat(selected_tables).convert_dtypes()
+
+# Rename columns if necessary
+new_column_names = {
+     "No. overall": "Number",
+     "No. in season": "# in season",
+}
+data = south_park_data.rename(columns=new_column_names)
+
+# Find all columns that contain the phrase 
+air_date_columns = [col for col in data.columns if "Original air date" in col]
+prod_code_columns = [col for col in data.columns if "Prod. code " in col]
+viewers_columns = [col for col in data.columns if "Viewers" in col or "viewers" in col]
+
+
+# Verify the identified columns
+print("Identified air date columns:", air_date_columns)
+print("Identified Prod. code columns:", prod_code_columns)
+print("Identified viewers_columns columns:", viewers_columns)
+
+# Combine these columns into a single column
+# For each row, select the first non-null value from the original  columns
+data['Air Date'] = data[air_date_columns].bfill(axis=1).iloc[:, 0]
+data['Prod Code'] = data[prod_code_columns].bfill(axis=1).iloc[:, 0]
+data['Viewers (millions)'] = data[viewers_columns].bfill(axis=1).iloc[:, 0]
+
+# Drop the original air date columns with footnote indicators
+data = data.drop(columns=air_date_columns)
+data = data.drop(columns=prod_code_columns)
+data = data.drop(columns="U.S. viewers (millions)")
+# Display the DataFrame's column names to confirm
+print(data.columns)
+data
